740447e6cd79b8e999eea2f608b1e9107212edc0
nrtopping <nrtopping@gmail.com>
2020-05-20 23:46:21 -0700

    [Setup] Switch logging, update workflow to use correct output

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function run() {
     const token = core.getInput('github-token');
     const title = core.getInput('title');
     const body = core.getInput('body');
-    console.log(`Creating issue with label ${label}...`);
+    core.info(`Creating issue with label ${label}...`);
 
     const octokit = new github.GitHub(token);
 
@@ -19,8 +19,6 @@ async function run() {
     });
 
     const issues = await octokit.paginate(opts);
-    console.log('Found open issues with tag:');
-    console.log(issues);
     if (issues == null || issues.length == 0) {
       const { data: issue } = await octokit.issues.create({
         owner: github.context.repo.owner,
@@ -29,15 +27,16 @@ async function run() {
         title: title,
         body: body
       })
-      console.log('created new issue:');
-      console.log(issue);
+      core.info(`created new issue ${issue.number}`);
+      core.debug(issue);
 
       core.setOutput('issue-number', issue.number);
       return;
     } else if (issues.length > 1) {
       throw `Too many ${label} issues created - expect 0 or 1`
     }
-
+    core.info(`Found existing issue ${issues[0].number}`);
+    core.debug(issues);
     core.setOutput('issue-number', issues[0].number);
   } catch(error) {
     core.setFailed(error.message);
@@ -46,4 +45,3 @@ async function run() {
 
 console.log('Starting...');
 run();
-console.log('Ending...');
